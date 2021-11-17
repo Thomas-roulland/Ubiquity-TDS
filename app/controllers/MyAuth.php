c415cf533c1ed2989a351add30f5b041be6d6593
Thomas Roulland <troulland@sio.local>
2021-11-17 10:48:56 +0100

    delete, edit marche + ajout connexion, sauvegarde d'une liste en cour

---
+++ app/controllers/MyAuth.php
@@ -0,0 +1,105 @@
+<?php
+namespace controllers;
+use Ubiquity\attributes\items\router\Get;
+use Ubiquity\attributes\items\router\Post;
+use Ubiquity\cache\CacheManager;
+use Ubiquity\utils\http\UResponse;
+use Ubiquity\utils\http\USession;
+use Ubiquity\utils\http\URequest;
+use controllers\auth\files\MyAuthFiles;
+use Ubiquity\controllers\auth\AuthFiles;
+use Ubiquity\attributes\items\router\Route;
+
+#[Route(path: "/login",inherited: true,automated: true)]
+class MyAuth extends \Ubiquity\controllers\auth\AuthController{
+
+
+    protected $footerView = "@activeTheme/main/vFooter.html";
+    protected $headerView = "@activeTheme/main/vHeader.html";
+
+    private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle',array $buttons=[]) {
+        $this->loadView('main/message.html', compact('header', 'type', 'icon', 'message','buttons'));
+    }
+
+	protected function onConnect($connected) {
+		$urlParts=$this->getOriginalURL();
+		USession::set($this->_getUserSessionKey(), $connected);
+		if(isset($urlParts)){
+			$this->_forward(implode("/",$urlParts));
+		}else{
+			UResponse::header('location', '/');
+		}
+	}
+
+	protected function _connect() {
+		if(URequest::isPost()){
+			$email=URequest::post($this->_getLoginInputName());
+			$password=URequest::post($this->_getPasswordInputName());
+			$key='datas/users'.md5($email);
+            if(CacheManager::$cache->exists($key)){
+                $userInfos=CacheManager::$cache->fetch($key);
+                if ($userInfos['login']===$email && URequest::password_verify($this->_getPasswordInputName(), $userInfos['password'] )){
+                    return $email;
+                }
+            }
+		}
+		return;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
+	 */
+	public function _isValidUser($action=null) {
+		return USession::exists($this->_getUserSessionKey());
+	}
+
+	public function _getBaseRoute() {
+		return '/login';
+	}
+	
+	protected function getFiles(): AuthFiles{
+		return new MyAuthFiles();
+	}
+
+
+    protected function finalizeAuth()
+    {
+        if (! URequest::isAjax()) {
+            $this->loadView($this->footerView);
+        }
+    }
+
+    protected function initializeAuth()
+    {
+        if (! URequest::isAjax()) {
+            $this->loadView($this->headerView);
+        }
+    }
+
+	#[Get(path: "newUser",name: "myAuth.newUserForm")]
+	public function newUserForm(){
+		
+		$this->loadView('MyAuth/newUserForm.html');
+
+	}
+
+
+    public function _displayInfoAsString()
+    {
+        return true;
+    }
+
+    #[Post(path: "newUser",name: "myAuth.newUser")]
+	public function newUser(){
+        $email=URequest::post('email');
+        $key='datas/users'.md5($email);
+		if (!CacheManager::$cache->exists($key)){
+            CacheManager::$cache->store($key,['login'=>$email,'password'=>URequest::password_hash('password')]);
+            $this->showMessage('Création de compte', "votre compte a été créé avec l'email <b>$email</b>", 'succes');
+        }else{
+            $this->showMessage('Création de compte', "Compte déjà associé a l'email : <b>$email</b>", 'error', 'user');
+        }
+	}
+
+}
--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -4,6 +4,8 @@ use Ajax\JsUtils;
 use Ubiquity\attributes\items\router\Get;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\attributes\items\router\Post;
+use Ubiquity\controllers\auth\AuthController;
+use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\controllers\Router;
 use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\USession;
@@ -14,21 +16,41 @@ use Ubiquity\utils\http\USession;
   */
 class TodosController extends \controllers\ControllerBase{
 
+    use WithAuthTrait{
+     initialize as initAuth;
+    }
+
+    public function initialize()
+    {
+        $this->initAuth();
+        if(!URequest::isAjax()) {
+            $this->jquery->getOnClick('._toDelete', Router::path('todos.deleteElement'), '._content', ['hasLoader' => 'internal', 'attr' => 'data-value','listenerOn'=>'body']);
+        }
+    }
 
     const CACHE_KEY = 'datas/lists/';
     const EMPTY_LIST_ID='not saved';
     const LIST_SESSION_KEY='list';
     const ACTIVE_LIST_SESSION_KEY='active-list';
 
-    #[Get(path: "/default/",name: "home")]
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle',array $buttons=[]) {
+        $this->loadView('main/message.html', compact('header', 'type', 'icon', 'message','buttons'));
+    }
+
+    #[Get(path: "#/_default/",name: "home")]
 	public function index(){
         $list=USession::get(self::ACTIVE_LIST_SESSION_KEY, []);
         $this->jquery->click('._toEdit', 'let item=$(this).closest("div.item");
                                                             item.find("form").toggle();
                                                             item.find(".checkbox").toggle();');
-        $this->jquery->getOnClick('._toDelete', Router::path('todos.deleteElement'),'._content',['hasLoader'=>'internal', 'attr'=>'data-value']);
         $this->jquery->getHref('a', parameters: ['hasLoader' =>false, 'historize'=>false]);
-        $this->jquery->postOn('submit','._formEdit',Router::path('todos.editElement'), '{id: $(this).find("input").attr("id")}','._content', ['hasLoader'=>'internal']);
+        $this->jquery->postOn('submit','._formEdit',Router::path('todos.editElement'), '{id: $(this).find("input").attr("id"), element: $(this).find("input").val()}','._content', ['hasLoader'=>'internal']);
         $this->jquery->renderView('TodosController/index.html', ['list'=>$list]);
 	}
 
@@ -36,25 +58,24 @@ class TodosController extends \controllers\ControllerBase{
 	public function addElement(){
         $this->jquery->postFormOnClick('button',Router::path('todos.loadListFromForm'), 'frm','._content', ['hasLoader'=>'internal']);
 		$this->jquery->renderView('TodosController/addElement.html');
-
 	}
 
 
 	#[Get(path: "todos/delete/{index}",name: "todos.deleteElement")]
 	public function deleteElement($index=0){
-
-        $list=USession::get(self::ACTIVE_LIST_SESSION_KEY,[]);
+        $list=USession::get(self::ACTIVE_LIST_SESSION_KEY, []);
         unset($list[$index]);
         USession::set(self::ACTIVE_LIST_SESSION_KEY,\array_values($list));
         $this->index();
-
 	}
 
 
-	#[Post(path: "todos/edit",name: "todos.editElement")]
-	public function editElement(){
-
-        USession::addValueToArray(self::ACTIVE_LIST_SESSION_KEY, URequest::post('items'));
+	#[Post(path: "todos/edit/{index}",name: "todos.editElement")]
+	public function editElement($index=0){
+        $list=USession::get(self::ACTIVE_LIST_SESSION_KEY, []);
+        $list[URequest::post('id')]=URequest::post('element');
+        USession::set(self::ACTIVE_LIST_SESSION_KEY,\array_values($list));
+        $this->index();
         echo "edit effectuée";
 
 
@@ -93,4 +114,8 @@ class TodosController extends \controllers\ControllerBase{
 
 	}
 
+    protected function getAuthController(): AuthController
+    {
+        return new MyAuth($this);
+    }
 }
